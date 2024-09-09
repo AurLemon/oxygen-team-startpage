6e900bcc1b56a8e7872226f1ab7249182dfc7eb1
AurLemon <aurlemoncn@gmail.com>
2024-09-09 17:35:36 +0800

    Simply site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="name">
+          <div className="name-title">Oxygen Studio Project</div>
+          <div className="name-subtitle">项目导航站</div>
+        </div>
+        <div className="links">
+          <div className="link">
+            <a href="https://space.bilibili.com/390831920k">B 站</a>
+          </div>
+          <div className="link">
+            <a href="https://www.hydcraft.cn">氢气工艺</a>
+          </div>
+          <div className="link">
+            <a href="https://github.com/Oxygen-Studio">GitHub</a>
+          </div>
+        </div>
       </header>
+      <footer className="App-footer">
+        <a href="https://beian.miit.gov.cn/">
+          闽ICP备2023007345号-2
+        </a>
+      </footer>
     </div>
   );
 }
